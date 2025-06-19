846cf802ff5770e96a0cfdd9e0fbdcd534950d3e
MOHAMMED ADNAN A <120554745+mel0ca@users.noreply.github.com>
2025-06-19 20:15:03 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+</div>
+
+<script src="https://code.jquery.com/jquery-2.2.4.js" integrity="" crossorigin=""></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js
+"></script>
+</body>
+</html>
